79c47e363f6ef29222635a7be2d08a42bb4fcf6a
Satish Madamsetty <Satish@Satishs-MacBook-Pro.local>
2018-08-05 17:28:14 +0800

    all projects

---
+++ MyProject/js/IndiaClock.js
@@ -0,0 +1,9 @@
+<script type="text/javascript" language="JavaScript">
+obj=new Object;
+obj.clockfile="5005-green.swf";
+obj.TimeZone="GMT0530"; 
+obj.width=120;
+obj.height=40;
+obj.wmode="transparent";
+showClock(obj);
+</script>
---
+++ MyProject/js/login.js
@@ -0,0 +1,26 @@
+	var attempt = 5; 
+// Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "Satish" && password == "satish")
+{
+window.location = "Application.html"; // Redirecting to other page.
+return false;
+}
+else
+{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+window.location = "Error.html";
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
+}
+
